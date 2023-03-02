699458fb9f57e6002f37ebbab0629f231fe0c223
VadimKhokhlov13 <68466747+VadimKhokhlov13@users.noreply.github.com>
2023-03-02 14:08:48 +0300

    Update 1.php

--- 1.php
+++ 1.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 
 /**
  * @charset UTF-8
@@ -20,62 +20,62 @@
 
 // Список районов
 $areas = array (
-		1 => '5-й поселок',
-		2 => 'Голиковка',
-		3 => 'Древлянка',
-		4 => 'Заводская',
-		5 => 'Зарека',
-		6 => 'Ключевая',
-		7 => 'Кукковка',
-		8 => 'Новый сайнаволок',
-		9 => 'Октябрьский',
-		10 => 'Первомайский',
-		11 => 'Перевалка',
-		12 => 'Сулажгора',
-		13 => 'Университетский городок',
-		14 => 'Центр',
+	1 => '5-й поселок',
+	2 => 'Голиковка',
+	3 => 'Древлянка',
+	4 => 'Заводская',
+	5 => 'Зарека',
+	6 => 'Ключевая',
+	7 => 'Кукковка',
+	8 => 'Новый сайнаволок',
+	9 => 'Октябрьский',
+	10 => 'Первомайский',
+	11 => 'Перевалка',
+	12 => 'Сулажгора',
+	13 => 'Университетский городок',
+	14 => 'Центр',
 );
 
 // Близкие районы, связь осуществляется по индентификатору района из массива $areas
 $nearby = array (
-		1 => array(12,11),
-		2 => array(5,7,6,8),
-		3 => array(11,13),
-		4 => array(10,9,12),
-		5 => array(2,6,7,8),
-		6 => array(5,2,7,8),
-		7 => array(2,5,6,8),
-		8 => array(6,2,7,5),
-		9 => array(10,14),
-		10 => array(9,14,12),
-		11 => array(13,3,1,12),
-		12 => array(1,10),
-		13 => array(11,1,12),
-		14 => array(9,10),
+	1 => array(2,11),	
+	2 => array(12,3,6,8),
+	3 => array(11,13),    
+	4 => array(10,9,13), 
+	5 => array(2,6,7,8),   
+	6 => array(10,2,7,8),
+	7 => array(2,6,8),	
+	8 => array(6,2,7,12),	
+	9 => array(10,14),     
+	10 => array(9,14,12), 
+	11 => array(13,1,9),
+	12 => array(1,10),     
+	13 => array(11,1,8),	
+	14 => array(9,10),     
 );
 
 // список сотрудников
 $workers = array (
-		0 => array (
-				'login' => 'login1',
-				'area_name' => 'Октябрьский',
-		),
-		1 => array (
-				'login' => 'login2',
-				'area_name' => 'Зарека',
-		),
-		2 => array (
-				'login' => 'login3',
-				'area_name' => 'Сулажгора',
-		),
-		3 => array (
-				'login' => 'login4',
-				'area_name' => 'Древлянка',
-		),
-		4 => array (
-				'login' => 'login5',
-				'area_name' => 'Центр',
-		),
+	0 => array (
+			'login' => 'login1',
+			'area_name' => 'Октябрьский', //9
+	),
+	1 => array (
+			'login' => 'login2',
+			'area_name' => 'Зарека', //5
+	),
+	2 => array (
+			'login' => 'login3',
+			'area_name' => 'Сулажгора', //12
+	),
+	3 => array (
+			'login' => 'login4',
+			'area_name' => 'Древлянка', //3
+	),
+	4 => array (
+			'login' => 'login5',
+			'area_name' => 'Центр', //14
+	),
 );
 
 function findWorker($areaName) {
@@ -87,15 +87,16 @@ function findWorker($areaName) {
     } else {
     	$areaIndex = array_search($areaName, $areas);
         $nerbyIndexes = $nearby[$areaIndex];
+        $nearestWokers = [];
         foreach ($nerbyIndexes as $index) {
         	$nerbyName = $areas[$index];
         	if (isset($areasByWorkerIndex[$nerbyName])) {
             	$workerIndex = $areasByWorkerIndex[$nerbyName];
-    			return $workers[$workerIndex]['login'];
+    			$nearestWokers[] = $workers[$workerIndex]['login'];
             }
         }
+        return count($nearestWokers) ? $nearestWokers : null;
     }
-    return null;
 }
 
 ?>
